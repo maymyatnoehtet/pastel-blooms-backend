3257b56cd2058c322b4d0695e02464b4834571dc
maymyatnoehtet <may.mn.htet@gmail.com>
2023-08-01 18:23:05 +1000

    initialising user model and routes

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
