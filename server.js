468adc9bbbcc4722c20be77196f84e668187f22e
maymyatnoehtet <116809927+maymyatnoehtet@users.noreply.github.com>
2023-08-01 18:38:34 +1000

    Create server for app

--- server.js
+++ server.js
@@ -1,28 +1,10 @@
-const express = require("express");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
-const app = express();
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
 
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Connect to MongoDB
-// const mongoURI = "YOUR_MONGODB_CONNECTION_STRING";
-// mongoose.connect(mongoURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// mongoose.connection.on("connected", () => {
-//   console.log("Connected to MongoDB");
-// });
-
-// // Your API routes will go here (e.g., CRUD operations for the User model)
-
-// // Start the server
-// const port = 5000; // You can change the port as needed
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
